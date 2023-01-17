be5a6f3d9d755c02701593ac16aa7e31128b18e7
Janvier <jnahimana@gmail.com>
2023-01-17 23:50:22 +0200

    fix: Put every file in appropriate folder

--- Buckets/Business/Models/Validations/BucketDtoValidator.cs
+++ Buckets/Business/Models/Validations/BucketDtoValidator.cs
@@ -7,8 +7,6 @@ namespace CSharp_intro_1.Models.Validators
     {
         public BucketDtoValidator() {
             RuleFor(bucket => bucket.Title).NotEmpty().Length(5, 20).WithMessage("Please specify bucket name with minumum of 5 characters");
-            
-
         }
     }
 }
