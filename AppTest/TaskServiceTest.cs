73fe23e8cff29619da99915aff79c1c0447b2e19
Janvier Nahimana <jnahimana@objectivity.co.uk>
2023-01-25 21:49:45 +0200

    refactor: Rename people directory to person

--- AppTest/TaskServiceTest.cs
+++ AppTest/TaskServiceTest.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharp_intro_1.Models;
+using CSharp_intro_1.Repositories;
+using CSharp_intro_1.Services;
+using CSharp_intro_1.Services.interfaces;
+using Moq;
 
 namespace App.Tests
 {
-    internal class TaskServiceTest
+    public class TaskServiceTest
     {
+        private readonly Mock<IRepository<TaskDto>> _taskRepoMock;
+        private readonly Mock<ITaskRepository> _ItaskRepoMock;
+        private readonly IBucketService _bucketService;
+        private readonly IPersonService _personService;
+        private readonly TaskService _taskService;
+
+        public TaskServiceTest()
+        {
+            _taskService = new TaskService(_taskRepoMock.Object, _ItaskRepoMock.Object, _bucketService, _personService);
+
+        }
+        public void AllTask_GetAllTasks_ReturnListOfTasks()
+        {
+            _taskRepoMock = new Mock<IRepository<TaskDto>>();
+        }
     }
 }
